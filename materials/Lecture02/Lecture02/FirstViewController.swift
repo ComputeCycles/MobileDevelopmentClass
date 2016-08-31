fccfc35c1d85f57ff87c04629831ef03a1460a31
Van Simmons <rvs606@g.harvard.edu>
2016-08-30 20:45:32 -0400

    Adding assignment 2

---
+++ materials/Lecture02/Lecture02/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Lecture02
+//
+//  Created by Van Simmons on 8/30/16.
+//  Copyright © 2016 ComputeCycles, LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
