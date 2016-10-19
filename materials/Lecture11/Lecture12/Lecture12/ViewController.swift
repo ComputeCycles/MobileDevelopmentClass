796433c1af81f4b926b789392433d816a5699c8a
Van Simmons <rvsrvs@gmail.com>
2016-10-18 21:57:24 -0400

    Lecture 11 and 12

--- materials/Lecture11/Lecture12/Lecture12/ViewController.swift
+++ materials/Lecture11/Lecture12/Lecture12/ViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    var greenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after 
+        // loading the view, typically from a nib.
+        scrollView.contentSize = CGSize(width: 1000.0,
+                                        height: 1000.0)
+        let redView = UIView(frame: CGRect(x: 50.0,
+                                           y: 50.0,
+                                           width: 200.0,
+                                           height: 200.0))
+        redView.backgroundColor = UIColor.red
+        scrollView.addSubview(redView)
+        
+        greenView = UIView(frame: CGRect(x: 200.0,
+                                           y: 200.0,
+                                           width: 100.0,
+                                           height: 300.0))
+        greenView.backgroundColor = UIColor.green
+        scrollView.addSubview(greenView)
+    
+        let yellowView = UIView(frame: CGRect(x: 50.0,
+                                             y: 50.0,
+                                             width: 50.0,
+                                             height: 50.0))
+        yellowView.backgroundColor = UIColor.yellow
+        greenView.addSubview(yellowView)
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +48,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func step(_ sender: AnyObject) {
+        greenView.bounds = CGRect(x: greenView.bounds.origin.x,
+                                  y: greenView.bounds.origin.y - 10.0,
+                                  width: greenView.bounds.size.width,
+                                  height: greenView.bounds.size.height)
+    }
 
+    @IBAction func stepFrame(_ sender: AnyObject) {
+        greenView.frame = CGRect(x: greenView.frame.origin.x,
+                                  y: greenView.frame.origin.y - 10.0,
+                                  width: greenView.frame.size.width,
+                                  height: greenView.frame.size.height)
+    }
 }
 
